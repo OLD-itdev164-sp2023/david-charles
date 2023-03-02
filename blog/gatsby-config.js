f7535acff28218a2bef29582145bd6462374900c
David Charles <charled1@gmatc.matc.edu>
2023-03-02 14:26:33 -0600

    enviornment variables created

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -1,12 +1,6 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
-
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+})
 module.exports = {
   siteMetadata: {
     title: `Loop Worx`,
