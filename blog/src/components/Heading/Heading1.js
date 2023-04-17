b8974f34e2e3beec6b7ddeb2f2d2201d6c0cdc1b
David Charles <charled1@gmatc.matc.edu>
2023-04-17 12:44:57 -0500

    Completed Activity 7

--- blog/src/components/Heading/Heading1.js
+++ blog/src/components/Heading/Heading1.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BaseHeading } from './BaseHeading'
 
-cont Heading1 = props =>
-    <BaseHeading as='h1' fontSize={[3,4,5]} {...props} />
+const Heading1 = props =>
+    <BaseHeading as='h1' fontSize={[3, 4, 5]} {...props} />
 
     export { Heading1 as H1 }
